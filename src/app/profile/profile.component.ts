4f89b5d3223de5725708fe5de8317d944917ffcf
Mosesvalei <moses.mwanzia@student.moringaschool.com>
2021-08-09 13:51:55 +0300

    class

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService} from '../profile.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
