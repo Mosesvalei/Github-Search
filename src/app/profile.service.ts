09396215e2a705565229291947c5f8675fe3574c
Mosesvalei <moses.mwanzia@student.moringaschool.com>
2021-08-09 13:21:58 +0300

    service component

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -3,4 +3,19 @@ import { Injectable } from '@angular/core';
 import { map} from 'rxjs/operators';
 import { environment } from '../environments/environment';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  private username: string;
+  private clientid = 'f8b2761f67f82e2acbce';
+  private clientsecret = '8150f0085b5c4006a53bb8d1e42e889930cb60b2';
+
+
+  constructor(private http: HttpClient) {
+    console.log('service is now ready');
+    this.username ='Mosesvalei';
+  }
+
 
